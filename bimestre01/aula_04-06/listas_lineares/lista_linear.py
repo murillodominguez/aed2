83ad0dfe31e6569674b783690d12f2853c2a7eb1
murillodominguez <murillo.v.carballo@gmail.com>
2026-04-14 02:06:18 -0300

    retrabalho aula 13 de abr.

--- bimestre01/aula_04-06/listas_lineares/lista_linear.py
+++ bimestre01/aula_04-06/listas_lineares/lista_linear.py
@@ -23,31 +23,78 @@ class ListaLinear:
         print("INICIO E FIM:",self.ini,self.fim)
 
     def inserir(self, pos: int, node: int):
-        if (pos <= 0 or pos > self.tamanho() + 1) or (self.ini == 0 and self.fim == self.max - 1):
+        tamanho = self.tamanho()
+        if (pos <= 0 or pos > tamanho + 1) or (self.ini == 0 and self.fim == self.max - 1):
             return False
 
         if self.is_vazio():
             self.ini = 0
             self.fim = 0
-
-        # if self.ini != 0 and self.fim != self.max - 1:
-        #     go_left = False
-        #     go_right = False
-
-        #     elem_to_the_left = self.ini
         
+        elif self.fim != self.max - 1 and self.ini != 0:
+            go_left = False
+            go_right = False
+            print("Inserindo!")
+            center = tamanho // 2 + 1
+            if pos <= center:
+                print("Vai esquerda!")
+                go_left = True
+            else:
+                go_right = True
+            
+            if go_right:
+                for i in range(self.fim, self.ini + pos - 2, -1):
+                    self.vetor[i+1] = self.vetor[i]
+                self.fim += 1
+
+            if go_left:
+                for i in range(self.ini, self.ini + pos - 1):
+                    self.vetor[i-1] = self.vetor[i]
+                self.ini -= 1
+
         elif self.fim != self.max - 1:
             for i in range(self.fim, self.ini + pos - 2, -1):
                 self.vetor[i+1] = self.vetor[i]
-                print(i)
             self.fim += 1
         else:
             for i in range(self.ini, self.ini + pos - 1):
                 self.vetor[i-1] = self.vetor[i]
-                print(i)
             self.ini -= 1
-            print(node)
-            print(self.vetor)
-        print(self.ini + pos - 1)
+
         self.vetor[self.ini + pos - 1] = node
         return True
+    
+    def remover(self, pos):
+        if self.is_vazio() or (pos > self.tamanho() or pos <= 0):
+            return False
+        
+        if pos != 1 and pos != self.tamanho():
+            center = self.tamanho() // 2
+            if pos <= center:
+                for i in range(self.ini + pos - 2, self.ini - 1, -1):
+                    self.vetor[i+1] = self.vetor[i]
+                    if i == self.ini:
+                        self.vetor[i] = None
+                self.ini += 1
+            else:
+                for i in range(self.ini + pos, self.fim + 1):
+                    self.vetor[i-1] = self.vetor[i]
+                    if i == self.fim:
+                        self.vetor[i] = None
+                self.fim -= 1
+        elif pos == 1:
+            self.vetor[self.ini + pos - 1] = None
+            self.ini += 1
+            if self.tamanho() == 0:
+                self.ini = -1
+                self.fim = -1
+        else:
+            self.vetor[self.ini + pos - 1] = None
+            self.fim -= 1
+        
+        return True
+
+                    
+            
+        
+    
